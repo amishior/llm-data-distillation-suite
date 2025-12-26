01c3b87b86f03e1a6936c657a58d44ebdf33ff3e
amishor <42690923+amishior@users.noreply.github.com>
2025-12-26 22:00:35 +0800

    Delete sft_distill/.ipynb_checkpoints directory

--- sft_distill/.ipynb_checkpoints/__init__-checkpoint.py
+++
@@ -1,9 +0,0 @@
-# sft_distill/__init__.py
-
-from .config import CONFIG_TEMPLATE
-from .pipeline import DatasetDistillationPipeline
-
-__all__ = [
-    "CONFIG_TEMPLATE",
-    "DatasetDistillationPipeline",
-]
